25ae1da520ae93ef5364710a9894f41a1f8c4e92
Md Shahabuddin <mdshahabuddin7987@gmail.com>
2023-09-12 00:50:53 +0530

    Main Method is Loaded

--- Calci.java
+++ Calci.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 interface Calci
 {
   int sum(int n1, int n2);
@@ -7,5 +8,18 @@ class SubClass implements Calci
 public int sum(int n1, int n2)
   {
 		return n1 + n2;
+	}
+}
+class Driver {
+	public static void main(String[] args)
+   {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 Numbers to Find Sum");
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		SubClass i1 = new SubClass();
+		int sum = i1.sum(n1, n2);
+		System.out.println(n1 + "+" + n2 + " = " + sum);
+		sc.close();
 	}
 }
